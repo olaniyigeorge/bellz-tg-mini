f24d685f536bdbd06f0eb1de2917f3d305630548
Olaniyi George <olaniyigeorge77@gmail.com>
2024-10-22 15:52:32 +0100

    types

---
+++ utils/telegram.d.ts
@@ -0,0 +1,16 @@
+interface TelegramWebApp {
+    initData: string;
+    initDataUnsafe: {
+      user: TelegramUser;
+      [key: string]: any; // Include other properties as necessary
+    };
+    close: () => void;
+    isExpanded: boolean;
+    [key: string]: any; // Include other properties as necessary
+  }
+  
+  interface Window {
+    Telegram: {
+      WebApp: TelegramWebApp;
+    };
+  }
