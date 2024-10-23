9d6e9da7cf464f2f0ba1c3feba4daea5e2842239
Olaniyi George <olaniyigeorge77@gmail.com>
2024-10-23 15:13:01 +0100

    Auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
+import TelegramAuth from "@/components/telegramAuth";
 import { getSession } from "@/utils/session";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function Home() {
-  const session = getSession()
+export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-3xl font-bold">
+        <h1 className="text-3xl font-extrabold">
           Bellz Mini App
         </h1>
         <ol className="text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
@@ -42,7 +42,7 @@ export default async function Home() {
           </Link>
 
         </div>
-        <pre>{JSON.stringify({session, null, 2})</pre>
+
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
